c10b11b14f70f3ccb4232a9dcd35b55664c0f193
Marcin Twardak <twardakm@gmail.com>
2015-03-26 13:19:31 +0100

    LED functions, 72 MHz

---
+++ PinDefines.h
@@ -0,0 +1,28 @@
+#ifndef PINDEFINES_H_
+#define PINDEFINES_H_
+
+/*
+ * LEDs
+ */
+#define LED1_GPIO	GPIOC
+#define LED1_PIN	GPIO_Pin_14
+#define	LED2_GPIO	GPIOC
+#define LED2_PIN	GPIO_Pin_15
+#define LED3_GPIO	GPIOC
+#define LED3_PIN	GPIO_Pin_13
+#define LED4_GPIO	GPIOB
+#define LED4_PIN	GPIO_Pin_6
+#define LED5_GPIO	GPIOB
+#define LED5_PIN	GPIO_Pin_7
+#define LED6_GPIO	GPIOA
+#define LED6_PIN	GPIO_Pin_12
+#define LED7_GPIO	GPIOA
+#define LED7_PIN	GPIO_Pin_11
+#define LED8_GPIO	GPIOA
+#define LED8_PIN	GPIO_Pin_8
+#define LED9_GPIO	GPIOC
+#define LED9_PIN	GPIO_Pin_9
+#define LED10_GPIO	GPIOC
+#define LED10_PIN	GPIO_Pin_8
+
+#endif
